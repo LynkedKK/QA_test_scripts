ba555af0e5864a2a302e6ce3f3b2dc2cd56e5d71
louiscklaw <louiscklaw@gmail.com>
2020-11-20 01:04:56 +0800

    update and test,

--- tests/UI_test/functional/test_happyflow_1_click_accept_and_continue/test_happyflow_1_chrome_click_accept_and_continue.py
+++ tests/UI_test/functional/test_happyflow_1_click_accept_and_continue/test_happyflow_1_chrome_click_accept_and_continue.py
@@ -33,6 +33,8 @@ from jp import *
 SELENIUM_HUB_HOST='localhost'
 FOOD_PAGE='http://menymeny.com/food/%E3%82%84%E3%81%8D%E3%81%A8%E3%82%8A/'
 
+ACTUAL_SCREENSHOT='reports/UI_test/functional/test_happyflow_1_click_accept_and_continue/test_happyflow_1_chrome_first_time_landing_after_click_accept_iphonex.png'
+EXPECTED_SCREENSHOT='tests/UI_test/functional/test_happyflow_1_click_accept_and_continue/expected_result/test_happyflow_1_chrome_first_time_landing_after_click_accept_iphonex.png'
 
 def test_happyflow_1_chrome_click_accept_and_continue():
   selenium_url = 'http://{}:4444/wd/hub'.format(SELENIUM_HUB_HOST)
@@ -46,19 +48,17 @@ def test_happyflow_1_chrome_click_accept_and_continue():
     desired_capabilities = chrome_options.to_capabilities()
   )
 
-  URL = 'http://192.168.88.105:8002/'
-  browser.get(URL)
-  # browser.get(FOOD_PAGE)
+  # URL = 'http://192.168.88.105:8002/'
+  # browser.get(URL)
+  browser.get(FOOD_PAGE)
   sleep(1)
 
   fl_page = first_time_landing.MainPage(browser)
-  fl_page.checkLinkExist()
   fl_page.checkAcceptAndContinueButtonExist()
-  fl_page.takeScreenshot('/tmp/t1.png')
-  # browser.find_element_by_id('test_link')
-  # browser.find_element_by_xpath('//*[@id="test_link"]')
-
+  fl_page.takeScreenshot(ACTUAL_SCREENSHOT)
   # fl_page.checkLinkExist()
 
   browser.quit()
   sleep(30)
+
+  assertSameImage(EXPECTED_SCREENSHOT, ACTUAL_SCREENSHOT,0.1,  'first time landing test after clicking accept failed')
--- tests/lib/pages/first_time_landing.py
+++ tests/lib/pages/first_time_landing.py
@@ -1,7 +1,9 @@
 import os,sys
 from pprint import pprint
+from selenium.webdriver.common.by import By
 
-ACCEPT_AND_CONTINUE_XPATH='//*[@id="tac-container"]/div[2]/div[5]/input'
+
+ACCEPT_AND_CONTINUE_XPATH='//*[@value="同意して予約する"]'
 
 def helloworld():
   print('helloworld')
@@ -15,8 +17,10 @@ class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
 
+    def takeScreenshot(self, sc_filename):
+        self.driver.save_screenshot(sc_filename)
 
-class CheckPage(BasePage):
+class MainPage(BasePage):
     """Home page action methods come here. I.e. Python.org"""
 
     #Declares a variable that will contain the retrieved text
@@ -25,10 +29,15 @@ class CheckPage(BasePage):
       print('helloworld')
 
     def checkLinkExist(self):
-      self.find_element_by_id('test_link')
+      self.driver.find_element_by_id('test_link')
+      self.driver.find_element_by_xpath('//*[@id="test_link"]')
 
     def checkAcceptAndContinueButtonExist(self):
-      self.find_element_by_xpath(ACCEPT_AND_CONTINUE_XPATH)
+      ELE_ACCEPT_BUTTON = self.driver.find_element_by_xpath(ACCEPT_AND_CONTINUE_XPATH)
+      ELE_ACCEPT_BUTTON.click()
+      # self.find_element(By.XPATH, ACCEPT_AND_CONTINUE_XPATH)
+
+      # self.find_element_by_xpath(ACCEPT_AND_CONTINUE_XPATH)
 
     # def is_title_matches(self):
     #     """Verifies that the hardcoded text "Python" appears in page title"""
