50583e5c96e26815132bc7d2210ae9ce6d3ecd1d
louiscklaw <louiscklaw@gmail.com>
2020-11-19 14:12:30 +0800

    update flow filename,

--- docs/flows/user_flow/build.sh
+++ docs/flows/user_flow/build.sh
@@ -13,30 +13,30 @@ export PATH=$PATH:$USER_FLOW/node_modules/.bin
 
 # npm install
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-1/happy-flow-1.mmd -o $FLOWS_DIR/user_flow/happyflow-1/happy-flow-1.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-1/happyflow-1.mmd -o $FLOWS_DIR/user_flow/happyflow-1/happyflow-1.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-2/happy-flow-2.mmd -o $FLOWS_DIR/user_flow/happyflow-2/happy-flow-2.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-2/happyflow-2.mmd -o $FLOWS_DIR/user_flow/happyflow-2/happyflow-2.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-3/happy-flow-3.mmd -o $FLOWS_DIR/user_flow/happyflow-3/happy-flow-3.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-3/happyflow-3.mmd -o $FLOWS_DIR/user_flow/happyflow-3/happyflow-3.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-4/happy-flow-4.mmd -o $FLOWS_DIR/user_flow/happyflow-4/happy-flow-4.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-4/happyflow-4.mmd -o $FLOWS_DIR/user_flow/happyflow-4/happyflow-4.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-5/happy-flow-5.mmd -o $FLOWS_DIR/user_flow/happyflow-5/happy-flow-5.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-5/happyflow-5.mmd -o $FLOWS_DIR/user_flow/happyflow-5/happyflow-5.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-6/happy-flow-6.mmd -o $FLOWS_DIR/user_flow/happyflow-6/happy-flow-6.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-6/happyflow-6.mmd -o $FLOWS_DIR/user_flow/happyflow-6/happyflow-6.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-7/happy-flow-7.mmd -o $FLOWS_DIR/user_flow/happyflow-7/happy-flow-7.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-7/happyflow-7.mmd -o $FLOWS_DIR/user_flow/happyflow-7/happyflow-7.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-8/happy-flow-8.mmd -o $FLOWS_DIR/user_flow/happyflow-8/happy-flow-8.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-8/happyflow-8.mmd -o $FLOWS_DIR/user_flow/happyflow-8/happyflow-8.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-9/happy-flow-9.mmd -o $FLOWS_DIR/user_flow/happyflow-9/happy-flow-9.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-9/happyflow-9.mmd -o $FLOWS_DIR/user_flow/happyflow-9/happyflow-9.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-10/happy-flow-10.mmd -o $FLOWS_DIR/user_flow/happyflow-10/happy-flow-10.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-10/happyflow-10.mmd -o $FLOWS_DIR/user_flow/happyflow-10/happyflow-10.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-11/happy-flow-11.mmd -o $FLOWS_DIR/user_flow/happyflow-11/happy-flow-11.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-11/happyflow-11.mmd -o $FLOWS_DIR/user_flow/happyflow-11/happyflow-11.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-12/happy-flow-12.mmd -o $FLOWS_DIR/user_flow/happyflow-12/happy-flow-12.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-12/happyflow-12.mmd -o $FLOWS_DIR/user_flow/happyflow-12/happyflow-12.png &
 
-mmdc -i $FLOWS_DIR/user_flow/happyflow-13/happy-flow-13.mmd -o $FLOWS_DIR/user_flow/happyflow-13/happy-flow-13.png &
+mmdc -i $FLOWS_DIR/user_flow/happyflow-13/happyflow-13.mmd -o $FLOWS_DIR/user_flow/happyflow-13/happyflow-13.png &
 
 wait
