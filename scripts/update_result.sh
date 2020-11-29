2ae1450fb9a97be498ee2c0b471aca59f640290c
louiscklaw <louiscklaw@gmail.com>
2020-11-30 02:36:07 +0800

    working update,

--- scripts/update_result.sh
+++ scripts/update_result.sh
@@ -6,10 +6,10 @@ TMP_DIR=$(mktemp -d)
 # git clone --depth 1 git@github.com:LynkedKK/QA_test_result.git $TMP_DIR
 git clone --depth 1 https://$GITHUB_TOKEN@github.com/LynkedKK/QA_test_result.git $TMP_DIR
 
-cp -r /home/logic/_workspace/LynkedKK_QA_Offical_repo/reports/functional/ $TMP_DIR
+cp -r reports/functional/ $TMP_DIR
 
-pushd $TMP_DIR
+cd $TMP_DIR
   git add .
   git commit -m"update result,"
   git push
-popd
+cd -
